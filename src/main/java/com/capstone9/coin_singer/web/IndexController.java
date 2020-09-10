3b9d2ca43645b7ed9b58e72dc136d6a1b1d139bc
jimkim1 <oceanbelow@naver.com>
2020-09-10 10:31:05 +0900

    프로젝트 구조 초기 setting

---
+++ src/main/java/com/capstone9/coin_singer/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.capstone9.coin_singer.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
